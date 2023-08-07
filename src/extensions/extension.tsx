285d4fdbba54488ea089d92d70e64f6f51bc5a59
Konsl <82901383+Konsl@users.noreply.github.com>
2023-08-08 01:16:03 +0200

    Remove unused extension file

--- src/extensions/extension.tsx
+++
@@ -1,8 +0,0 @@
-(async () => {
-  while (!Spicetify?.showNotification) {
-    await new Promise(resolve => setTimeout(resolve, 100));
-  }
-
-  // Show message on start.
-  Spicetify.showNotification("Welcome!");
-})()
